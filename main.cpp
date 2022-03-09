4c866d15c93e96e888bf90502db5aab43b7d2eef
sschwei1 <24sebastian05@gmail.com>
2022-03-09 19:20:57 +0100

    add basic sfml window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    sf::VideoMode window_bounds(800, 600);
+    std::string window_title = "Hello World!";
+    unsigned framerate_limit = 120;
+    bool vertical_sync_enabled = true;
+
+    auto window = new sf::RenderWindow(window_bounds, window_title);
+    window->setFramerateLimit(framerate_limit);
+    window->setVerticalSyncEnabled(vertical_sync_enabled);
+
+    sf::RectangleShape background(sf::Vector2f(window_bounds.height, window_bounds.width));
+    background.setFillColor(sf::Color::Green);
+
+    sf::Event sfEvent;
+
+    while(window->isOpen()) {
+        // handle sfml events
+        while (window->pollEvent(sfEvent)){
+            if(sfEvent.type == sf::Event::Closed){
+                window->close();
+            }
+        }
+
+        // update logic
+
+        // render
+        window->clear(sf::Color::Green);
+        window->display();
+    }
+
+    // memory cleanup
+    delete window;
+
     return 0;
 }
