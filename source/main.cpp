1e0d83f17a741e48f784e9ac653f941a6dc1dc3a
sschwei1 <24sebastian05@gmail.com>
2022-03-09 23:47:39 +0100

    add config file

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <fstream>
 
 int main() {
-    sf::VideoMode window_bounds(800, 600);
+    // default settings
     std::string window_title = "Hello World!";
+    sf::VideoMode window_bounds(200, 200);
     unsigned framerate_limit = 120;
     bool vertical_sync_enabled = true;
 
+    // read config
+    std::ifstream ifs("config/window.ini");
+    if(ifs.is_open()){
+        std::getline(ifs, window_title);
+        ifs >> window_bounds.width >> window_bounds.height;
+        ifs >> framerate_limit;
+        ifs >> vertical_sync_enabled;
+    }
+    ifs.close();
+
+    // initialise window
     auto window = new sf::RenderWindow(window_bounds, window_title);
     window->setFramerateLimit(framerate_limit);
     window->setVerticalSyncEnabled(vertical_sync_enabled);
-
-    sf::RectangleShape background(sf::Vector2f(window_bounds.height, window_bounds.width));
-    background.setFillColor(sf::Color::Green);
 
     sf::Event sfEvent;
 
